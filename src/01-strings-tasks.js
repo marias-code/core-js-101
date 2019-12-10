14ab2d9b1dea25a804a6a803c8533aab02d1d9f1
Masha <nov93523@gmail.com>
2019-12-10 11:35:55 -0500

    feat: add solution for extractNameFromTemplate function

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -66,8 +66,9 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const extractedName = value.split(', ').pop();
+  return extractedName.substring(0, extractedName.length - 1);
 }
 
 
